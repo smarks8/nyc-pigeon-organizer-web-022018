c1f6298bbee10ddf01fa2107d234e3fa47e7b76a
Stacey  Marks <staceyamarks@gmail.com>
2018-02-07 03:20:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_hash = {}
+  data.each do |category, options_hash|
+    options_hash.each do |option, name_array|
+      name_array.each do |name|
+        if !new_hash.include?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].include?(category)
+          new_hash[name][category] = []
+        end
+        if !new_hash[name][category].include?(option)
+          new_hash[name][category] << option.to_s
+        end 
+      end
+    end
+  end
+  new_hash
+end 
